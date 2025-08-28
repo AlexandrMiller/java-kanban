f491d8995157eed81978e895e876e380bcd1951c
AlexMiller <miller.alex.rob@yandex.ru>
2025-08-28 14:06:08 +0500

    sprint8.5

---
+++ src/out/test/java-kanban/InMemoryTaskManagerTest.java
@@ -0,0 +1,47 @@
+import manager.InMemoryTaskManager;
+import manager.Managers;
+import manager.TaskManager;
+import model.Epic;
+import model.SubTask;
+import model.Task;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import util.enumConstant.*;
+
+
+
+
+
+/*
+class InMemoryTaskManagerTest extends InMemoryTaskManager {
+    private TaskManager taskManager;
+
+    @BeforeEach
+    void setUp() {
+    taskManager = Managers.getDefault();
+    }
+
+    @Test
+    void testManagerCreation() {
+        Task task = new Task("task","decr",Status.NEW);
+        taskManager.createTask(task);
+        assertNotNull(task);
+        assertEquals(1,task.getId());
+
+        Epic epic = new Epic("epic","decr");
+        taskManager.createEpic(epic);
+        assertNotNull(epic);
+        assertEquals(2,epic.getId());
+
+        SubTask subTask = new SubTask("subtask","decr",Status.NEW,epic.getId());
+        taskManager.createSubTask(subTask);
+        assertNotNull(subTask);
+        assertEquals(3,subTask.getId());
+
+    }
+
+
+
+}
+
